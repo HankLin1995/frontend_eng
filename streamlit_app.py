e259207f07d5ba0c39a022c98be24e607260a479
YLIANET\zong-han01273 <apple84026113@gmail.com>
2025-05-06 16:57:22 +0800

    add login

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,19 +8,26 @@ st.set_page_config(page_title=f"施工抽查系統-V{VERSION}",layout="wide")
 
 st.logo("lOGO.JPG")
 
-# 建築巡檢系統頁面
-project_page = st.Page("views/projects.py", title="專案管理", icon=":material/domain:")
-inspection_page = st.Page("views/inspections.py", title="抽查表清單", icon=":material/list:")
-photo_page = st.Page("views/photos.py", title="照片圖廊", icon=":material/image:")
-inspection_add_page = st.Page("views/inspections_add.py", title="新增抽查表", icon=":material/create:")
-inspection_edit_page= st.Page("views/inspections_edit.py", title="編輯抽查表", icon=":material/edit_document:")
+if not st.user.is_logged_in:
+    if st.sidebar.button("Google 登入",type="primary"):
+        st.login()
+else:
+    if st.sidebar.button(f"{st.user.name}登出",type="secondary"):
+        st.logout()
 
-pg=st.navigation(
-    {
-        "設定": [project_page],
-        "異動": [inspection_add_page,inspection_edit_page],
-        "總覽": [inspection_page,photo_page]
-    }
-)
+    # 建築巡檢系統頁面
+    project_page = st.Page("views/projects.py", title="專案管理", icon=":material/domain:")
+    inspection_page = st.Page("views/inspections.py", title="抽查表清單", icon=":material/list:")
+    photo_page = st.Page("views/photos.py", title="照片圖廊", icon=":material/image:")
+    inspection_add_page = st.Page("views/inspections_add.py", title="新增抽查表", icon=":material/create:")
+    inspection_edit_page= st.Page("views/inspections_edit.py", title="編輯抽查表", icon=":material/edit_document:")
 
-pg.run()
+    pg=st.navigation(
+        {
+            "設定": [project_page],
+            "異動": [inspection_add_page,inspection_edit_page],
+            "總覽": [inspection_page,photo_page]
+        }
+    )
+
+    pg.run()
